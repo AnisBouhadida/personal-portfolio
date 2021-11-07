9480755f4b1d91184811974d18e67179361073f8
Anis Bouhadida <anisdoescode@outlook.fr>
2021-11-07 16:34:33 +0100

    init node server & refactore directories & finish form submit and emailing

---
+++ public/main.js
@@ -0,0 +1,42 @@
+// Add smooth scrolling
+$(function () {
+  $("#toAboutMe").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// Form submition
+
+// Defining the sendMail method that calls the FetchAPI to send data to back-end
+const sendMail = (mail) => {
+  fetch("/contact", {
+    method: "post",
+    body: mail,
+  }).then((response) => {
+    return response.json();
+  });
+};
+
+// Adding the event listener
+const form = document.getElementById("contact-form");
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let mail = new FormData(form);
+
+  sendMail(mail);
+});
